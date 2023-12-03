ecbb5d204a6609b315a6ab0465a504d1f666eeb0
Shan_shanHY <shan_shanhy@qq.com>
2023-12-03 11:47:26 +0800

    -添加额外出站列表 -重构WebUI -添加自定义输出文件名

--- c2box.py
+++ c2box.py
@@ -1,8 +1,14 @@
 import os
-import json
-import yaml
-import requests
+import re
+import sys
+import time
 from urllib.parse import unquote, urlparse
+
+import json
+import json5
+import requests
+import yaml
+
 import convert
 
 
@@ -14,7 +20,7 @@ def build_outbound(clash_subs_list):
              "interrupt_exist_connections": False}]}
     for clash_sub in clash_subs_list:
         try:
-            req = requests.get(url=clash_sub, headers={'User-Agent': 'clashmeta'})
+            req = requests.get(url=clash_sub, headers={'User-Agent': 'clash meta'})
             clash_sub_name = unquote(req.headers.get("content-disposition"))
             if clash_sub_name:
                 clash_sub_name = clash_sub_name.split("''")[1]
@@ -51,6 +57,9 @@ def build_outbound(clash_subs_list):
             outbounds_list.append(outbound)
         outbounds["outbounds"].extend(outbounds_list)
         print(f"[{clash_sub_name}]转换完成，共转换了{len(outbounds["outbounds"])}条协议")
+    if len(outbounds) == 0:
+        print("未转换任何协议，停止运行")
+        return False
     outbounds["outbounds"] += [
         {"type": "direct", "tag": "direct"},
         {"type": "block", "tag": "block"},
@@ -58,42 +67,87 @@ def build_outbound(clash_subs_list):
     return outbounds
 
 
-def main():
-    clash_subs = input("输入订阅链接，多条订阅使用 | 隔开")
+def main(output):
+    clash_subs = input("输入订阅链接，多条订阅使用 | 隔开：\n")
     clash_subs_list = clash_subs.split("|")
 
     template_list = os.listdir("./template")
+    template = dict()
     if len(template_list) == 0:
         print("未找到模板文件，仅生成outbounds")
-        template = dict()
     else:
-        if len(template_list) > 1:
-            print("找到多个模板文件，请选择需要的模板：")
-            while True:
-                for i, each in enumerate(template_list):
-                    print(f"{i}. {each}")
-                try:
-                    template_name = template_list[int(input("请输入模板序号: "))]
+        # 选择文件名
+        print("找到模板文件，请选择需要的模板(输入为空跳过)：")
+        while True:
+            for i, each in enumerate(template_list):
+                print(f"{i}. {each}")
+            try:
+                template_num = input("请输入模板序号: ")
+                if len(template_num) == 0:
+                    template_name = ""
+                    print("跳过模板选择，仅生成 outbounds")
                     break
-                except ValueError:
-                    print("错误的输入!")
-                except IndexError:
-                    print("序号不存在!")
-        else:
-            template_name = template_list[0]
-        try:
-            with open(f"./template/{template_name}", "r", encoding="UTF-8") as f:
-                template = json.load(f)
-        except json.decoder.JSONDecodeError:
-            print(f"模板文件 {template_name} 格式错误！")
-            exit(1)
-        print(f"已加载模板文件 {template_name}")
+                else:
+                    template_name = template_list[int(template_num)]
+                break
+            except ValueError:
+                print("错误的输入!")
+            except IndexError:
+                print("序号不存在!")
+        # 根据文件名加载文件
+        if len(template_name) > 0:
+            try:
+                with open(f"./template/{template_name}", "r", encoding="UTF-8") as f:
+                    template = json5.load(f)
+            except Exception:
+                print(f"模板文件 {template_name} 格式错误！")
+                exit(1)
+            print(f"已加载模板文件 {template_name}")
 
     outbounds = build_outbound(clash_subs_list)
+    if not outbounds:
+        return False
+    outbounds = extra_outbound(outbounds)
     sing_box_conf = convert.base.merge_dict(template, outbounds)
-    with open("./output/config.json", 'w', encoding="UTF-8") as f:
+    with open(f"./output/{output}", 'w', encoding="UTF-8") as f:
         json.dump(sing_box_conf, f, ensure_ascii=False, indent=4)
 
 
+def extra_outbound(outbounds, group_list=None):
+    if not group_list:
+        try:
+            with open("./group.json", "r", encoding="utf-8") as f:
+                group_list = json5.load(f)
+        except Exception:
+            print("额外出站分组配置加载错误，跳过生成")
+            return outbounds
+    for key, value in group_list.items():
+        if value.get("type") in ["selector", "urltest"]:
+            outbound = {"type": value["type"], "tag": key, "outbounds": [],
+                        "interrupt_exist_connections": False}
+        else:
+            print('仅支持以"selector"/"urltest"作为出站对象')
+            return outbounds
+        if isinstance(value.get("outbounds"), list):
+            for key1 in value.get("outbounds"):
+                for key2 in outbounds["outbounds"]:
+                    if key2["tag"] not in ["direct", "dns_out"] and re.search(key1, key2["tag"]):
+                        outbound["outbounds"].append(key2["tag"])
+        elif isinstance(value.get("outbounds"), str):
+            key1 = value.get("outbounds")
+            for key2 in outbounds["outbounds"]:
+                if key2["tag"] not in ["direct", "dns_out"] and re.search(key1, key2["tag"]):
+                    outbound["outbounds"].append(key2["tag"])
+        if len(outbound["outbounds"]) > 0:
+            outbounds["outbounds"].append(outbound)
+    return outbounds
+
+
 if __name__ == "__main__":
-    main()
+    filename = f"config_{str(int(time.time()))}.json"
+    args = sys.argv
+    for count, key in enumerate(args):
+        if key.startswith("-"):
+            if key[1:] == "o":
+                filename = args[count + 1]
+    main(filename)
--- web.py
+++ web.py
@@ -1,53 +1,87 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from urllib.parse import urlparse, parse_qs
-import requests
+import base64
 import json
+import re
+import time
+from urllib.parse import urlparse
+
+import json5
+import requests
+from flask import Flask, render_template, request, abort, Response
+
 import c2box
-import convert
 
-class RequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        parsed_url = urlparse(self.path)
-        query_params = parse_qs(parsed_url.query)
+app = Flask(__name__, template_folder='./html')
 
-        if parsed_url.path == "/convert":
-            if "sub" in query_params:
-                outbounds = c2box.build_outbound(query_params["sub"])
-                if "template" in query_params:
-                    try:
-                        req = requests.get(url=query_params["template"][0])
-                        template = req.json()
-                    except:
-                        self.send_response(401)
-                        self.send_header('Content-type', 'text/plain')
-                        self.end_headers()
-                        self.wfile.write("Get Template Fail".encode())
-                        return
-                    outbounds = convert.base.merge_dict(template, outbounds)
-                self.send_response(200)
-                self.send_header('Content-type', 'application/octet-stream')
-                self.send_header('Content-Disposition', 'attachment; filename=config.json')
-                self.end_headers()
-                self.wfile.write(json.dumps(outbounds, ensure_ascii=False, indent=4).encode())
+
+@app.route('/')
+def run():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def build_url():
+    parsed_url = urlparse(request.url)
+    url = f"{parsed_url.scheme}://{parsed_url.netloc}/convert?"
+
+    # 处理订阅
+    sub = re.split(r"\||\r\n", request.form['sub'])
+    for i in sub:
+        url += f"sub={i}&"
+
+    # 处理模板
+    template = base64.urlsafe_b64encode(request.form['template'].encode()).decode(encoding="utf-8")
+    url += f"template={template}&"
+
+    # 处理额外出站组
+    outbound_group = base64.urlsafe_b64encode(request.form['outbound_group'].encode()).decode(encoding="utf-8")
+    url += f"outbound_group={outbound_group}&"
+
+    return render_template('index.html', url=url[:-1], sub=request.form['sub'],
+                           template=request.form['template'], outbound_group=request.form['outbound_group'])
+
+
+@app.route('/convert')
+def convert():
+
+    # 处理订阅链接
+    sub = request.args.getlist('sub')
+    outbound = c2box.build_outbound(sub)
+    if not outbound:
+        abort(412, "No available proxy in sub")
+
+    # 处理额外出站分组文件
+    if len(request.args.get('outbound_group', [""][0])) > 0:
+        try:
+            outbound_group = json5.loads(base64.urlsafe_b64decode(request.args.get('outbound_group', [""][0])).decode(encoding="utf-8"))
+            outbound = c2box.extra_outbound(outbound, outbound_group)
+        except Exception:
+            abort(412, "Outbound group get fail")
+
+        # 处理模板文件
+    if len(request.args.get('template', [""][0])) > 0:
+        template = base64.urlsafe_b64decode(request.args.get('template', [""][0])).decode(encoding="utf-8")
+        try:
+            if template.startswith("http"):
+                template_url = template
+                req = requests.get(url=template_url)
+                template = json5.loads(req.text)
             else:
-                self.send_response(404)
-                self.send_header('Content-type', 'text/plain')
-                self.end_headers()
-                self.wfile.write("No Clash Subscribe".encode())
+                template = json5.loads(template)
+            template["outbounds"] = outbound["outbounds"]
+            outbound = template
+        except Exception:
+            abort(412, "Template get fail")
 
-        else:
-            self.send_response(404)
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            self.wfile.write("Not Found".encode())
+    config_download = json.dumps(outbound, ensure_ascii=False, indent=4)
+    response = Response(config_download, content_type="application/json")
 
-
-def run(server_class=HTTPServer, handler_class=RequestHandler, port=8080):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print(f"Starting server on port {port}")
-    httpd.serve_forever()
+    # 自定义下载文件名
+    conf_name = request.args.get("conf_name", "")
+    if len(conf_name) == 0:
+        conf_name = f"config_{str(int(time.time()))}.json"
+    response.headers.set("Content-Disposition", f"attachment; filename={conf_name}")
+    return response
 
 
 if __name__ == '__main__':
-    run()
+    app.run(debug=True)
